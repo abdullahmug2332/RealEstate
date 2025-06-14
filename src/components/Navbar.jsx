6b39bb4544ba28746ab608a950b7c5297a6175d4
abdullahmug2332 <abdullahmug2332@gmail.com>
2025-06-14 19:44:34 +0500

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { RxCross1 } from "react-icons/rx";
 export default function Navbar() {
     const [toggle, setToogle] = useState(false)
     return (
-        <>
+        <section className="overflow-x-hidden w-[100vw]">
             <nav className=" fixed top-0 left-0  w-full bg-[#101010]  p-2 md:p-0 overflow-hidden" >
                 <div className='w-[95%] md:w-[80%] mx-auto  flex items-center'>
                     <div className='flex items-center'>
@@ -41,6 +41,6 @@ export default function Navbar() {
 
                 </div>
             </div>
-        </>
+        </section>
     )
 }
