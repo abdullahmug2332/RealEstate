7e5418a07a070e3b6bc24111569583ca3598dc93
abdullahmug2332 <abdullahmug2332@gmail.com>
2025-06-14 19:29:20 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar/> 
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <section className='hero h-[100vh] min-h-[540px] '>
+      <div className='w-[90%] md:w-[80%] mx-auto items-start flex flex-col justify-center h-full gap-[20px]'>
+        <p className='text-white text-[40px] md:text-[50px] font-semibold leading-[50px] w-[100%] md:w-[59%]'>Find the best properties  deals ever.</p>
+        <p className='w-[100%] md:w-[50%]'> Discover the perfect place to call home — whether you're buying, renting, or investing. We help you find your dream property with ease, confidence, and expert support every step of the way.</p>
+        <div className="flex items-center gap-[10px] md:gap-[20px] mb-[50px]">
+        <button className='btn'>Contact</button>
+        <button className='btn '>View Properties</button>
+        </div>
+      </div>
+    </section>
+  )
+}
