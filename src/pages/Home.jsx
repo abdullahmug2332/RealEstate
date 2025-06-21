679a7d8c49ef09e4e6c083bb0df536066ea3b5f2
abdullahmug2332 <abdullahmug2332@gmail.com>
2025-06-21 19:46:18 +0500

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Home2 from '../components/Home2'
 import Whychooseus from '../components/Whychooseus'
+import Banner from '../components/Banner'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero/>
       <Home2/>
       <Whychooseus/>
+      <Banner />
     </section>
   )
 }
