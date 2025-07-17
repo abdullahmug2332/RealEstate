89629d8c52ecd88f0957f3ee7719a255523b0a20
abdullahmug2332 <abdullahmug2332@gmail.com>
2025-07-18 00:57:44 +0500

    add backend link

--- src/components/Property.jsx
+++ src/components/Property.jsx
@@ -115,7 +115,7 @@ export default function Property() {
                 isOpen={modalIsOpen}
                 onRequestClose={closeModal}
                 overlayClassName="fixed inset-0 bg-black bg-opacity-90 z-50 flex items-center justify-center"
-                className="outline-none w-full max-w-[90%] h-[90vh] relative"
+                className="outline-none w-full max-w-[90%] h-[90vh] relative mt-[160px]"
             >
                 <button onClick={closeModal} className="absolute top-20 right-4 text-white text-3xl z-50 text-[30px]"><RxCross1 className="text-white text-3xl z-50 text-[30px]" /></button>
                 <Swiper
@@ -131,7 +131,7 @@ export default function Property() {
                                 <img
                                     src={`${baseURL}/images/${item.src}`}
                                     alt={`modal-media-${index}`}
-                                    className="max-h-full max-w-full mx-auto object-contain"
+                                    className="max-h-[80vh] max-w-full mx-auto object-contain"
                                 />
                             ) : (
                                 <video controls autoPlay className="max-h-full  mx-auto max-w-full object-contain">
